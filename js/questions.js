a51065ab19e63beb6e76f2b870630dcca458a1dd
Mthabs <mthabs1@gmail.com>
2023-04-06 17:38:51 +0000

    initial commit

---
+++ js/questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which country won the first ever World Cup in 1930?",
+    answer: "Uruguay",
+    options: [
+      "England",
+      "USA",
+      "Uruguay",
+      "Argentina"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which country has won the most World Cups?",
+    answer: "Brazil",
+    options: [
+      "France",
+      "Brazil",
+      "Argentina",
+      "Germany"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which country has appeared in three World Cup finals, but never won the competition?",
+    answer: "Netherlands",
+    options: [
+      "Greece",
+      "Italy",
+      "Ukraine",
+      "Netherlands"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which country hosted 2010 world cup?",
+    answer: "South Africa",
+    options: [
+      "Ghana",
+      "South Africa",
+      "Mexico",
+      "South Korea"
+    ]
+  },
+    {
+    numb: 5,
+    question: "In which World Cup did Diego Maradona score his infamous 'Hand of God' goal?",
+    answer: "Mexico 1986",
+    options: [
+      "Mexico 1986",
+      "Italy 1968",
+      "Germany 1986",
+      "Argentina 1986"
+    ]
+  },
+];
