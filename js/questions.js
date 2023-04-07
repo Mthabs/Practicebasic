2af2809385c5af90eda1a18c95704c2939910e5d
Mthabs <mthabs1@gmail.com>
2023-04-07 20:38:39 +0000

    add questions

--- js/questions.js
+++ js/questions.js
@@ -55,4 +55,60 @@ let questions = [
       "Argentina 1986"
     ]
   },
+  
+    {
+    numb: 6,
+    question: "Which country won the first ever World Cup in 1930?",
+    answer: "Uruguay",
+    options: [
+      "England",
+      "USA",
+      "Uruguay",
+      "Argentina"
+    ]
+  },
+    {
+    numb: 7,
+    question: "Which country has won the most World Cups?",
+    answer: "Brazil",
+    options: [
+      "France",
+      "Brazil",
+      "Argentina",
+      "Germany"
+    ]
+  },
+    {
+    numb: 8,
+    question: "Which country has appeared in three World Cup finals, but never won the competition?",
+    answer: "Netherlands",
+    options: [
+      "Greece",
+      "Italy",
+      "Ukraine",
+      "Netherlands"
+    ]
+  },
+    {
+    numb: 9,
+    question: "Which country hosted 2010 world cup?",
+    answer: "South Africa",
+    options: [
+      "Ghana",
+      "South Africa",
+      "Mexico",
+      "South Korea"
+    ]
+  },
+    {
+    numb: 10,
+    question: "In which World Cup did Diego Maradona score his infamous 'Hand of God' goal?",
+    answer: "Mexico 1986",
+    options: [
+      "Mexico 1986",
+      "Italy 1968",
+      "Germany 1986",
+      "Argentina 1986"
+    ]
+  },
 ];
--- js/script.js
+++ js/script.js
@@ -153,13 +153,13 @@ function showResult(){
     quiz_box.classList.remove("activeQuiz"); //hide quiz box
     result_box.classList.add("activeResult"); //show result box
     const scoreText = result_box.querySelector(".score_text");
-    if (userScore > 3){ // if user scored more than 3
+    if (userScore > 8){ // if user scored more than 8
         //creating a new span tag and passing the user score number and total question number
         let scoreTag = '<span>Congratulations!!!, You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
         scoreText.innerHTML = scoreTag;  //adding new span tag inside score_Text
     }
-    else if(userScore > 1){ // if user scored more than 1
-        let scoreTag = '<span>Hard Luck, You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
+    else if(userScore > 5){ // if user scored more than 1
+        let scoreTag = '<span>Nice!!!, You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
         scoreText.innerHTML = scoreTag;
     }
     else{ // if user scored less than 1
