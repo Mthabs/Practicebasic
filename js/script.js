c32815f1ac6c0a56a855f8ead2aa566cb07666d3
Mthabs <mthabs1@gmail.com>
2023-04-08 10:42:33 +0000

    add instructions

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 //selecting all required elements
 const start_quiz = document.querySelector(".home_page button");
-const info_box = document.querySelector(".info_box");
-const exit_btn = info_box.querySelector(".buttons .quit");
-const continue_btn = info_box.querySelector(".buttons .restart");
+const instructions = document.querySelector(".instructions");
+const exit_btn = instructions.querySelector(".buttons .quit");
+const continue_btn = instructions.querySelector(".buttons .restart");
 const quiz_box = document.querySelector(".quiz_box");
 const result_box = document.querySelector(".result_box");
 const option_list = document.querySelector(".option_list");
@@ -12,17 +12,17 @@ const timeCount = document.querySelector(".timer .timer_sec");
 
 // if startQuiz button clicked
 start_quiz.onclick = ()=>{
-    info_box.classList.add("activeInfo"); //show info box
+    instructions.classList.add("activeInfo"); //show info box
 }
 
 // if exitQuiz button clicked
 exit_btn.onclick = ()=>{
-    info_box.classList.remove("activeInfo"); //hide info box
+    instructions.classList.remove("activeInfo"); //hide info box
 }
 
 // if continueQuiz button clicked
 continue_btn.onclick = ()=>{
-    info_box.classList.remove("activeInfo"); //hide info box
+    instructions.classList.remove("activeInfo"); //hide info box
     quiz_box.classList.add("activeQuiz"); //show quiz box
     showQuetions(0); //calling showQestions function
     queCounter(1); //passing 1 parameter to queCounter
@@ -149,7 +149,7 @@ function optionSelected(answer){
 }
 
 function showResult(){
-    info_box.classList.remove("activeInfo"); //hide info box
+    instructions.classList.remove("activeInfo"); //hide info box
     quiz_box.classList.remove("activeQuiz"); //hide quiz box
     result_box.classList.add("activeResult"); //show result box
     const scoreText = result_box.querySelector(".score_text");
