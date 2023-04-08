a165e8dd7cc00859e20a6fea6167fe1842953651
Mthabs <mthabs1@gmail.com>
2023-04-08 10:28:24 +0000

    add home button

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //selecting all required elements
-const start_btn = document.querySelector(".start_btn button");
+const start_quiz = document.querySelector(".home_page button");
 const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
 const continue_btn = info_box.querySelector(".buttons .restart");
@@ -11,7 +11,7 @@ const timeText = document.querySelector(".timer .time_left_txt");
 const timeCount = document.querySelector(".timer .timer_sec");
 
 // if startQuiz button clicked
-start_btn.onclick = ()=>{
+start_quiz.onclick = ()=>{
     info_box.classList.add("activeInfo"); //show info box
 }
 
